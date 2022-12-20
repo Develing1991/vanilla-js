b4666a850e0be059465c94a15afe7a2e6495c208
Develing1991 <completed0728@gmail.com>
2022-12-20 17:00:17 +0900

    state 생성 및 core수정

--- src/App.js
+++ src/App.js
@@ -3,10 +3,25 @@ import { Component } from "./core/core"
 export default class App extends Component{
   constructor(){
     super({
-      tagName: 'h1'
+      state: {
+        inputText: ''
+      }
     })
   }
   render() {
-    this.el.textContent = 'Hello~ Vanilla.js'
+    this.el.classList.add('search')
+    this.el.innerHTML = /*html*/`
+      <input/>
+      <button>Click</button>
+    `
+    const inputEl = this.el.querySelector('input')
+    inputEl.addEventListener('input', () => {
+      this.state.inputText = inputEl.value
+    })
+
+    const buttonEl = this.el.querySelector('button')
+    buttonEl.addEventListener('click', () => {
+      console.log(this.state.inputText);
+    })
   }
 }
